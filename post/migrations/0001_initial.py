dbd18c974ae80b2476311359621b72e4a09c9f2b
K1 <lord.k1.2009@gmail.com>
2015-11-23 20:45:44 +0330

    api has accomplished for first step

---
+++ post/migrations/0001_initial.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('title', models.CharField(max_length=100)),
+                ('created_date', models.DateTimeField(auto_now_add=True)),
+            ],
+            options={
+                'verbose_name_plural': 'Categories',
+                'verbose_name': 'Category',
+            },
+        ),
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('title', models.CharField(max_length=100)),
+                ('created_date', models.DateTimeField(auto_now_add=True)),
+                ('updated_date', models.DateTimeField(auto_now=True)),
+                ('category', models.ForeignKey(to='post.Category', related_name='posts')),
+            ],
+            options={
+                'verbose_name_plural': 'Posts',
+                'verbose_name': 'Post',
+            },
+        ),
+    ]
--- post/serializers.py
+++ post/serializers.py
@@ -4,13 +4,15 @@ from post.models import Category, Post
 __author__ = 'k1'
 
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['title', 'created_date']
-
-
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['title', 'created_date', 'updated_date']
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    posts = PostSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Category
+        fields = ['title', 'posts', 'created_date']
